58d98f8c1b33939ad533047fd291470508933f2b
ANAS <anaspappadan@gmail.com>
2024-10-01 10:55:34 +0530

    update dark theme on chart and invoice

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -28,8 +28,8 @@ export default async function RevenueChart({
         Recent Revenue
       </h2>
 
-      <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+      <div className="rounded-xl bg-gray-50 dark:bg-zinc-900 p-4">
+        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white dark:bg-zinc-800 p-4 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}>
